c15bccc3e9ceb8001901193c1f2af0ecab9b4673
KevinHung <kevinaugust145@gmail.com>
2024-07-27 15:26:16 +0800

    Initial Commit

---
+++ ChangeAppColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChangeAppColor
+//
+//  Created by 洪宗燦 on 2024/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChangeAppColorTests/ChangeAppColorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeAppColorTests.swift
+//  ChangeAppColorTests
+//
+//  Created by 洪宗燦 on 2024/7/27.
+//
+
+import Testing
+@testable import ChangeAppColor
+
+struct ChangeAppColorTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
