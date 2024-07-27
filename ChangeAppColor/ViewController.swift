384537e6317abe69d0ef40d51be4eb62799776af
KevinHung <kevinaugust145@gmail.com>
2024-07-27 23:39:53 +0800

    2024/07/27

--- ChangeAppColor/ViewController.swift
+++ ChangeAppColor/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+ 
+  
+    @IBOutlet weak var imageViewToBeChanged: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loadi
+ 
     }
-
-
+    
+    
+    @IBAction func ChangeImageViewBackGroundColor(_ sender: Any) {
+        imageViewToBeChanged
+        
+            .backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        
+        
+    }
+    
 }
-
+#Preview {
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    return storyboard.instantiateInitialViewController()!
+}
